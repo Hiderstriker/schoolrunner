eded5f026fd9e1a931efeeffcb7b40bb37a2994e
Hiderstriker <philip-nolte@web.de>
2022-04-28 22:14:18 +0200

    camera + sprint

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,72 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    public float speed =100.0f;
+    public Camera cam;
 
-    float keyX, keyY, keyZ;
+    public Transform headLoc;
+
+    public Transform orientation;
+
+    public float moveSpeed = 30.0f;
+    public float sprintSpeed = 60.0f;
+    private float speed;
+
+    public float sensX = 2.7f;
+    public float sensY = 2.7f;
+
+    float keyX, keyY;
+
+    float mouseX, mouseY;
+
+    float xRotation, yRotation;
+
+    Vector3 moveDirection;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
         keyX = Input.GetAxisRaw("Horizontal");
         keyY = Input.GetAxisRaw("Vertical");
-        keyZ = Input.GetAxisRaw("Jump");
+
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        cam.transform.localPosition = headLoc.localPosition;
+
+        yRotation += mouseX * sensX;
+        xRotation -= mouseY * sensY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.localRotation = Quaternion.Euler(0, yRotation, 0);
+
+        moveDirection = orientation.forward * keyY + orientation.right * keyX;
+
+        CheckSprint();
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(keyX * 50.0f, keyZ * 50.0f, keyY * 50.0f));
+        rb.AddForce(moveDirection.normalized * speed);
     }
+
+    private void CheckSprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = moveSpeed;
+        }
+    }
+
 }
