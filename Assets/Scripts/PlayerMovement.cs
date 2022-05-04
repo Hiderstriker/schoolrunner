2de7e767e91718dae267db2fd397603eea148f9f
Hiderstriker <philip-nolte@web.de>
2022-05-04 12:49:34 +0200

    Beschreibungen im Code hinzugefügt

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -29,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDirection;
 
 
+    //Cursor verschindet und wird in der Mitte gelockt wenn man auf den Screen clickt (automatisch eingestellt, dass er wieder erscheint, wenn man Esc drückt)
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -38,31 +39,40 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //laufen
         keyX = Input.GetAxisRaw("Horizontal");
         keyY = Input.GetAxisRaw("Vertical");
 
+        //Mausbewegung erkennen lassen
         mouseX = Input.GetAxisRaw("Mouse X");
         mouseY = Input.GetAxisRaw("Mouse Y");
 
+        //Kamera auf den "head" (child von Player) gebindet
         cam.transform.localPosition = headLoc.localPosition;
 
+        //Kamerabewegung and Sensetivity angepasst und Drehblockade eingebaut.
         yRotation += mouseX * sensX;
         xRotation -= mouseY * sensY;
         xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
 
+        //Eingestellt, dass sich andren Achsen mitdrehen, wenn man sich auf einer anderen Achse dreht
         cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.localRotation = Quaternion.Euler(0, yRotation, 0);
 
+        //Eingestellt, dass man sich immer in die Richtung bewegt in die man schaut
         moveDirection = orientation.forward * keyY + orientation.right * keyX;
 
+        //ermoeglicht das Sprinten
         CheckSprint();
     }
-
+    
+    //Bewegung
     private void FixedUpdate()
     {
         rb.AddForce(moveDirection.normalized * speed);
     }
 
+    //Sprinten auf LeftShift gelegt und die Schnelligkeit angepasst.
     private void CheckSprint()
     {
         if (Input.GetKey(KeyCode.LeftShift))
